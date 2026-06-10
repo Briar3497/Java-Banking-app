b5a671bb6bb0525bbce181bc8e3103a0697f76b3
Briar Lilly <silvermoon3497@gmail.com>
2026-06-10 11:02:18 -0500

    app initialized

---
+++ ImagineBanking/src/main/java/com/example/ImagineBanking/ImagineBankingApplication.java
@@ -0,0 +1,13 @@
+package com.example.ImagineBanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagineBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagineBankingApplication.class, args);
+	}
+
+}
---
+++ ImagineBanking/src/test/java/com/example/ImagineBanking/ImagineBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ImagineBanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagineBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
